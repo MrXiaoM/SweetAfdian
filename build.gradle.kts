992cc2f324d48d02afd6f2d9b0e2e7902efdebda
人間工作 <coolxiaom95@gmail.com>
2024-12-28 02:27:21 +0800

    exclude unused modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,18 @@ tasks {
         ).forEach { (original, target) ->
             relocate(original, "$shadowGroup.$target")
         }
+        listOf(
+            "top/mrxiaom/pluginbase/func/AbstractGui*",
+            "top/mrxiaom/pluginbase/func/gui/*",
+            "top/mrxiaom/pluginbase/utils/IA*",
+            "top/mrxiaom/pluginbase/utils/ItemStackUtil*",
+            "top/mrxiaom/pluginbase/func/GuiManager*",
+            "top/mrxiaom/pluginbase/gui/*",
+            "top/mrxiaom/pluginbase/func/LanguageManager*",
+            "top/mrxiaom/pluginbase/func/language/*",
+            "top/mrxiaom/pluginbase/utils/Adventure*",
+            "top/mrxiaom/pluginbase/utils/Bytes*",
+        ).forEach(this::exclude)
     }
     build {
         dependsOn(shadowJar)
