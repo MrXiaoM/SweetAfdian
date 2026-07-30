985916d448d3f1b3fc75fa8ee098b995d3a815e5
MrXiaoM <coolxiaom95@gmail.com>
2026-07-30 16:20:42 +0800

    update 1.0.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 val base = LibraryHelper(project)
 
 group = "top.mrxiaom.sweet.afdian"
-version = "1.0.10"
+version = "1.0.11"
 val targetJavaVersion = 8
 val pluginBaseModules = base.modules.run { listOf(library, actions) }
 val shadowGroup = "top.mrxiaom.sweet.afdian.libs"
